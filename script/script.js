bafaee2f31f9e52fb640b30448e8c318495d6c25
Nuthelk <nuthelkcd@gmail.com>
2022-06-28 21:23:21 -0500

    update js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 let nombrePlanetas = Array ('Tierra', 'Marte')
 let distancia = [6, 3, '67']
-const tamaño = []
+const tamaño = [20, 30]
 let i = 0
+
 
 console.log(nombrePlanetas)
 console.log(distancia)
@@ -16,3 +17,14 @@ while (i < nombrePlanetas.length) {
 for (let ind = 0; ind < nombrePlanetas.length; ind++){
     console.log(nombrePlanetas[ind])
 }
+
+nombrePlanetas.forEach((value, index, array)  => {
+    console.log('planeta', index, value)
+});
+
+nombrePlanetas.map((planetas, index) =>{ 
+    return(
+        console.log(`El planeta ${planetas} tiene tamaño de: ${tamaño[index]}`)
+    )
+})
+
