b72c983cbd3f142274a344022b5e05e57adc6f85
Nuthelk <nuthelkcd@gmail.com>
2022-06-28 21:10:43 -0500

    segundo commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let nombrePlanetas = Array ('Tierra', 'Marte')
+let distancia = [6, 3, '67']
+const tamaño = []
+let i = 0
+
+console.log(nombrePlanetas)
+console.log(distancia)
+console.log(tamaño)
+
+while (i < nombrePlanetas.length) {
+    console.log(i)
+    console.log(nombrePlanetas[i])
+    i++;
+}
+
+for (let ind = 0; ind < nombrePlanetas.length; ind++){
+    console.log(nombrePlanetas[ind])
+}
